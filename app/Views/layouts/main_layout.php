46e4c1efee680cc4e0e002c2cd0d174510d1f3f8
Ardon Reyes <ardonreyes26@gmail.com>
2026-09-22 04:22:00 +0800

    Follow up to last commit

--- app/Views/layouts/main_layout.php
+++ app/Views/layouts/main_layout.php
@@ -15,7 +15,7 @@
         </div>
         <div class="divider-nav"></div>
         <div class="links-nav">
-            <a href="<?= site_url('/home') ?>">Home</a>
+            <a href="<?= site_url('/') ?>">Home</a>
             <a href="<?= site_url('/about') ?>">About</a>
             <a href="<?= site_url('/customers') ?>">Customers</a>
             <a href="<?= site_url('/users') ?>">Users</a>
